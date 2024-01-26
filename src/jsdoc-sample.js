0d3895882b2bc3330bb7bae0a603d61872e30edf
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2024-01-27 02:53:43 +0330

    add jsdoc and docdash template

---
+++ src/jsdoc-sample.js
@@ -0,0 +1,9 @@
+/**
+ * Represents a book.
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ * @return {string} - book name
+ */
+function book(title, author) {
+  return 'test';
+}
